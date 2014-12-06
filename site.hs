c1e2cd389fbb6900be7f84aa5384f77d712c7300
Jose Manuel Calderon Trilla <jose@jmct.cc>
2014-12-06 11:59:46 -0500

    Mark 1

--- site.hs
+++ site.hs
@@ -3,17 +3,43 @@
 import Data.Monoid (mappend)
 import Data.String.Utils (replace)
 import Hakyll
+import qualified Data.Set as S
+import Text.Pandoc.Options
 
 main :: IO ()
 main = hakyll $ do
-    images
     index
+    about
     posts
+    memos
+    archivePosts
+    archiveMemos
     templates
+    images
     css
     atom
 
+-- Utility functions
 ------------------------------------------------------------------------------
+
+(<>) = mappend
+
+
+-- Use pandoc options to get typeset Mathematics
+------------------------------------------------------------------------------
+
+pandocMathCompiler = pandocCompilerWith defaultHakyllReaderOptions writerOpts
+  where
+    mathExtensions = [ Ext_tex_math_dollars, Ext_tex_math_double_backslash
+                     , Ext_latex_macros
+                     ]
+    defaultExt     = writerExtensions defaultHakyllWriterOptions
+    newExtensions  = foldr S.insert defaultExt mathExtensions
+    writerOpts     = defaultHakyllWriterOptions {
+                        writerExtensions = newExtensions, 
+                        writerHTMLMathMethod = MathJax ""
+                     }
+
 
 images :: Rules ()
 images = match "images/*" $ do
@@ -23,24 +49,68 @@ images = match "images/*" $ do
 templates :: Rules ()
 templates = match "templates/*" $ compile templateCompiler
 
+about :: Rules ()
+about = match "about.markdown" $ do
+            route $ setExtension "html"
+            compile $ pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= (return . removeIndexFromUrls)
+
+memos :: Rules ()
+memos = match "memos/*" $ do
+            route $ composeRoutes (composeRoutes (gsubRoute "memos/" (const "")) (gsubRoute ".md" (const "/index.md"))) (setExtension "html")
+            compile $ pandocMathCompiler
+                >>= loadAndApplyTemplate "templates/post.html" postCtx
+                >>= saveSnapshot "memoContent"
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
+
 posts :: Rules ()
 posts = match "posts/*" $ do
             route $ composeRoutes (composeRoutes (gsubRoute "posts/" (const "")) (gsubRoute ".md" (const "/index.md"))) (setExtension "html")
-            compile $ pandocCompiler
+            compile $ pandocMathCompiler
                 >>= loadAndApplyTemplate "templates/post.html" postCtx
                 >>= saveSnapshot "postContent"
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
+
+archivePosts :: Rules ()
+archivePosts = create ["posts.html"] $ do
+            route idRoute
+            compile $ do
+                posts <- recentFirst =<< loadAll "posts/*"
+                let ctx = constField "title" "Posts" <>
+                            listField "posts" postCtx (return posts) <>
+                            defaultContext
+                makeItem ""
+                    >>= loadAndApplyTemplate "templates/posts.html" ctx
+                    >>= loadAndApplyTemplate "templates/default.html" ctx
+                    >>= relativizeUrls
+
+archiveMemos :: Rules ()
+archiveMemos = create ["memos.html"] $ do
+            route idRoute
+            compile $ do
+                posts <- recentFirst =<< loadAll "memos/*"
+                let ctx = constField "title" "Memos" <>
+                            listField "posts" postCtx (return posts) <>
+                            defaultContext
+                makeItem ""
+                    >>= loadAndApplyTemplate "templates/posts.html" ctx
+                    >>= loadAndApplyTemplate "templates/default.html" ctx
+                    >>= relativizeUrls
 
 index :: Rules ()
 index = create ["index.html"] $ do
             route idRoute
             compile $ do
                 blogPosts <- recentFirst =<< loadAll "posts/*"
+                memoPosts <- recentFirst =<< loadAll "memos/*"
                 let indexContext = listField "posts" postCtx (return blogPosts) `mappend`
+                                   listField "memos" postCtx (return memoPosts) `mappend`
                                    defaultContext
                 makeItem "" >>= loadAndApplyTemplate "templates/index.html" indexContext
-                >>= loadAndApplyTemplate "templates/default.html" (constField "title" "Reid Draper" `mappend` defaultContext)
+                >>= loadAndApplyTemplate "templates/default.html" (constField "title" "JMCT" `mappend` defaultContext)
                 >>= (return . removeIndexFromUrls)
 
 
@@ -83,11 +153,11 @@ postCtx =
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration = FeedConfiguration
-    { feedTitle         = "Reid Draper's blog"
-    , feedDescription   = "Reid Draper's blog"
-    , feedAuthorName    = "Reid Draper"
-    , feedAuthorEmail   = "reiddraper@gmail.com"
-    , feedRoot          = "reiddraper.com"
+    { feedTitle         = "JMCT: FP"
+    , feedDescription   = "JMCT on functional programming"
+    , feedAuthorName    = "JMCT"
+    , feedAuthorEmail   = "jmct@jmct.cc"
+    , feedRoot          = "jmct.cc"
     }
 
 atom :: Rules ()
